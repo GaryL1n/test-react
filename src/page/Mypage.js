6c135bfec4eb84c8c016c3f65b496785c2c1c305
Gary <garylin0969@gmail.com>
2022-06-22 21:10:49 +0800

    0622

---
+++ src/page/Mypage.js
@@ -0,0 +1,30 @@
+function Mypage() {
+    return (
+        <div class="container">
+            <div class="row">
+                <div class="col-6">
+                    <div class="color-demo-area"></div>
+                </div>
+                <div class="col-6">
+                    <div class="color-selector-area d-flex flex-wrap">
+                        <div class="col-3">
+                            <div class="color-selector autobg"></div>
+                        </div>
+                        <div class="col-3">
+                            <div class="color-selector autobg"></div>
+                        </div>
+                        <div class="col-3">
+                            <div class="color-selector autobg"></div>
+                        </div>
+                        <div class="col-3">
+                            <div class="color-selector autobg"></div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+
+export default Mypage;
